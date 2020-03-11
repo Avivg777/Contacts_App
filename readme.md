f2bd5db93fdff94c5147e9ac9ee2066cd43c49d6
Aviv Golan <avig777@gmail.com>
2020-03-11 19:51:40 +0200

    Setup production build and server

--- readme.md
+++ readme.md
@@ -5,3 +5,12 @@ git status - View the changes to your projects.
 git add - Add files to staging area ("git add ." to add all files).
 git commit - Creates a new commit with files from staging area (git commit -m "Insert message here").
 git log - View recent commits.
+git remote add origin (insert repository link here) - adds a repository to the local project.
+git remote -v - View added origins.
+git push -u origin master - Pushes the local repo into the connected github repo.
+
+# Heroku Commands
+
+heroku login - Logs the app in to the heroku account.
+heroku --version - Checks the version of heroku on the machine.
+heroku create (optional app name) - creates a new app on heroku with a git repository.
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,56 @@
 const path = require('path');
+const CSSExtractPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/app.js',
-  devtool: 'eval-cheap-source-map',
-  devServer: {
-    contentBase: './public',
-    historyApiFallback: true
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
-  },
-  module: {
-    rules: [
-        {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: "babel-loader",
-            options: {
-                presets: ["@babel/preset-env", "@babel/preset-react"],
-                plugins: ["@babel/plugin-proposal-object-rest-spread"]
-            }
-        },
-        {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader',
-            ]
-        }
+module.exports = (env) => {
+  const isProduction = env === 'production';
+  const CSSExtract = new CSSExtractPlugin('styles.css');
+
+  return {
+    mode: isProduction ? 'production' : 'development',
+    entry: './src/app.js',
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: './public',
+      historyApiFallback: true
+    },
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'public'),
+    },
+    module: {
+      rules: [
+          {
+              test: /\.js$/,
+              exclude: /node_modules/,
+              loader: "babel-loader",
+              options: {
+                  presets: ["@babel/preset-env", "@babel/preset-react"],
+                  plugins: ["@babel/plugin-proposal-object-rest-spread"]
+              }
+          },
+          {
+              test: /\.s?css$/,
+              use: CSSExtract.extract({
+                use: [
+                  {
+                    loader: 'css-loader',
+                    options: {
+                      sourceMap: true
+                    }
+                  },
+                  {
+                    loader: 'sass-loader',
+                    options: {
+                      sourceMap: true
+                    }
+                  },
+                ]
+              })
+          }
+      ]
+    },
+    plugins: [
+      CSSExtract
     ]
   }
 };
